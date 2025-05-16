3deb12672d1973b185376a0e681258eaf1f44f46
Michael Irwin <mikesir87@gmail.com>
2025-05-15 22:10:09 -0400

    Create completed work and update README with "try it" instructions

--- README.md
+++ README.md
@@ -2,6 +2,33 @@
 
 This repository contains the materials for the "Port and build apps natively" lab.
 
+---
+
+**This branch represents the completed work**
+
+To try it out:
+
+1. Open a terminal in the `app` directory
+2. Run `docker compose up -d` to launch the dependencies
+3. Run `npm install` to install the app dependencies
+4. Run `npm run dev` to start the app
+5. Open [http://localhost:8080](http://localhost:8080), navigate to the topics, select the **products** topic, and open the **Messages** tab.
+6. Click the **Produce Message** and enter a new product message (example JSON below):
+
+    ```json
+    {
+      "action": "product_created",
+      "id": "product-51cb2813",
+      "name": "LucidFlow",
+      "description": "A bedside device utilizing subtle, dynamically shifting ambient light and binaural audio to gently guide you into deeper, more restorative sleep. Connects to your Microsoft 365 calendar to proactively adjust lighting and soundscapes based on your upcoming schedule, minimizing stress and promoting optimal rest."
+    }
+    ```
+
+7. Watch the app console output to see the generated email!
+
+
+---
+
 
 ## Table of contents
 
@@ -10,4 +37,3 @@ The lab is structured as a series of mini-lectures with small hands-on segments.
 1. [Running native and non-native containers and multi-container apps](./1-running-containers.md)
 2. [Writing an event-driven GenAI application that uses Docker Model Runner](./2-writing-genai-app.md)
 3. [Building multi-platform container images](./3-building-multi-platform-images.md)
-
--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,33 @@
 const { Kafka } = require('kafkajs');
 const OpenAI = require('openai');
 
+const openai = new OpenAI({
+  baseURL: process.env.OPENAI_BASE_URL || "http://localhost:12434/engines/v1",
+  apiKey: "not-required",
+});
+
+const SYSTEM_PROMPT = `You are a tool that is used to generate marketing emails to potential customers for a newly created product. You will be given the name and description of the product, and you will generate a marketing email to potential customers.
+        
+The result should be a JSON object with the following structure:
+{
+  "subject": "Subject line for email",
+  "body": "Body of email"
+}
+
+Do NOT include any additional information or explanations. Only the JSON object should be returned.
+
+The email MUST be less than 200 words in length. 
+The email MUST be in English and should be written in a friendly and engaging tone. 
+The email should include a call to action, such as "Click here to learn more" or "Order now". 
+The email should also include a subject line that is catchy and relevant to the product.
+The email should be relevant to the product and should not contain any irrelevant information. 
+The email should be written in a way that is easy to understand and should not contain any technical jargon. 
+The email should be written in a way that is engaging and should not be boring. 
+The email should be written in a way that is persuasive and should not be pushy. 
+The email should be written in a way that is friendly and should not be rude. 
+The email should be written in a way that is professional and should not be unprofessional.
+`;
+
 // Kafka configuration
 const kafka = new Kafka({
   clientId: 'ai-marketer',
@@ -33,11 +60,44 @@ const run = async () => {
 
 async function processMessage(data) {
   console.log('Received message:', data);
-  
-  // Do something with the received message
+
+  console.log('Generating email now');
+
+  // Generate sample marketing emails
+  const response = await openai.chat.completions.create({
+    model: process.env.OPENAI_MODEL || 'jacobhoward459/phi4-mini-instruct:3.84B-Q4_0',
+    messages: [
+      {
+        role: "system",
+        content: SYSTEM_PROMPT,
+      },
+      {
+        role: 'user',
+        content: `Product name: ${data.name}.\nProduct description: ${data.description}\n`,
+      },
+    ],
+  });
+
+  // Sometimes, models still wrap JSON in markdown code blocks, so let's remove that.
+  let responseText = response.choices[0].message.content;
+  if (responseText.startsWith('```json')) {
+    responseText = responseText.slice(7, -3);
+  }
+
+  // Simply output them to the console.
+  // In real situations, we'd look up relavent customers and queue emails.
+  const { subject, body } = JSON.parse(responseText);
+  console.log(`Subject: ${subject}`);
+  console.log(`Body: ${body}`);
 }
 
-run().catch(console.error);
+// run().catch(console.error);
+processMessage({
+  "action": "product_created",
+  "id": "product-51cb2813",
+  "name": "LucidFlow",
+  "description": "A bedside device utilizing subtle, dynamically shifting ambient light and binaural audio to gently guide you into deeper, more restorative sleep. Connects to your Microsoft 365 calendar to proactively adjust lighting and soundscapes based on your upcoming schedule, minimizing stress and promoting optimal rest."
+});
 
 ["SIGINT", "SIGTERM"].forEach((signal) => {
   process.on(signal, async () => {
